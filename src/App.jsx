8e155f7b9dd94e3b6419c7286975c583c4523fc6
KeyNun95 <k.nunez2017@gmail.com>
2023-07-17 18:10:45 -0400

    added skills array

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import SkillList from "./SkillList";
 import NewSkillForm from './NewSkillForm';
 import './App.css'
 
+const skills = [
+  { name: "HTML", level: 5 },
+  { name: "CSS", level: 3 },
+  { name: "JavaScript", level: 4},
+  { name: "Python", level: 2},
+];
+
 function App() {
   return (
     <div>
