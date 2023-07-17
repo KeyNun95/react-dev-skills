5cfb64393862122c6056781ef2b5cf6f3431255d
KeyNun95 <k.nunez2017@gmail.com>
2023-07-17 18:03:51 -0400

    part 1 complete

---
+++ src/NewSkillForm.jsx
@@ -0,0 +1,9 @@
+export default function NewSkillForm() {
+    return (
+        <form>
+            <label>Skill<input></input></label>
+            <label>Level<select><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select></label>
+            <button>ADD SKILL</button>
+        </form>
+    );
+}
