d3854cf942f5d41a7595da38c1fa13344974b548
KeyNun95 <k.nunez2017@gmail.com>
2023-07-18 01:10:55 -0400

    missing one css

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -5,7 +5,7 @@ export default function NewSkillForm() {
         <form>
             <label>Skill<input className="bar"></input></label>
             <label>Level<select className="dropdown"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option></select></label>
-            <button>ADD SKILL</button>
+            <button className="button">ADD SKILL</button>
         </form>
     );
 }
--- src/SkillListItem.jsx
+++ src/SkillListItem.jsx
@@ -1,5 +1,7 @@
+import "./SkillListItem.css";
+
 export default function SkillListItem({ skill }) {
     return (
-        <li>{skill.name} LEVEL {skill.level}</li>
+        <li className="devskill">{skill.name} LEVEL {skill.level}</li>
     );
 }
